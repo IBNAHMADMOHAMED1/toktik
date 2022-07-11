f662342319fc1465addd7b430e41fabb0d83f905
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-11 18:48:28 +0100

    Add NavBar content

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Link  from 'next/link'
+import { useRouter } from 'next/router'
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { AiOutlineMenu } from 'react-icons/ai'
+import BiSearch from 'react-icons/bi/'
+import { IoMdAdd } from 'react-icons/io'
+import Logo from '../utils/logo.png'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+      <div className=" w-full flex justify-between items-center border-b-2 border-gray-200 py-3 px-4 ">
+          <Link href="/" >
+              <div className="w-[100px] md:w-[130px] md:h-[30px]">
+                  <Image
+                      className="cursor-pointer" src={Logo} alt="logo" layout="responsive"  />                
+              </div>
+            </Link>
+    </div>
   )
 }
 
