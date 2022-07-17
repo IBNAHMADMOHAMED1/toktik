fa6a751a9d2c87681b6ec88c38736201680a3da4
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-17 19:57:44 +0100

    Add NoResults component for videos

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -4,7 +4,6 @@ import { MdOutlineVideocamOff } from 'react-icons/md';
 interface IProps {
   text: string;
 }
-
 const NoResults = ({ text }: IProps) => {
   return (
     <div className='flex flex-col justify-center items-center h-full w-full'>
