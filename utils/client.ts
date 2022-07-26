51ee6d983946fff384fe0e06f2f815d6c7493362
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-26 12:47:44 +0100

    Edit clinet Id sanity

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,6 @@
 import sanityClient from '@sanity/client';
-
 export const client = sanityClient({
-  projectId: '9sxrxo1l',
+  projectId: 'tp4ln3j8',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
