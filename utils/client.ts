3a57e4015f25b4c94be4eb153d5492dff437f46c
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-26 16:06:41 +0100

    Fix All Queries

--- utils/client.ts
+++ utils/client.ts
@@ -4,5 +4,5 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
-  token: "skubQFTC7DeATFfw4inOA4QM6LKsKKsA65Y3mifCoD6CsQu4QN9mPi1oG83kIahlWJTD2eDzmo6Q5qhPvG65o7ktPfQoge9rG3DpnJqJzWUHhLzVzu4aDqBpZCd7wZguIubBSSoTPTnTYH0sb28tTO0xqueP1pHpVZQsp8FesNpKjJEqOKXn",
+  token:"skubQFTC7DeATFfw4inOA4QM6LKsKKsA65Y3mifCoD6CsQu4QN9mPi1oG83kIahlWJTD2eDzmo6Q5qhPvG65o7ktPfQoge9rG3DpnJqJzWUHhLzVzu4aDqBpZCd7wZguIubBSSoTPTnTYH0sb28tTO0xqueP1pHpVZQsp8FesNpKjJEqOKXn",
 });
