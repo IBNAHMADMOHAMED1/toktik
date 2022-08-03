40145e761239745211b28602a3f401172b19430e
medandhala <medandhala@gmail.com>
2022-08-03 16:42:15 +0100

    Fix Page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,22 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { postDetailQuery } from '../../utils/queries';
 import { client } from '../../utils/client';
+import { uuid } from 'uuidv4';
 
-import { singleUserQuery, userCreatedPostsQuery, userLikedPostsQuery } from '../../utils/queries';
-// import { client } from '../../../utils/client:';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler( req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const { id } = req.query;
+    const query = postDetailQuery(id);
 
-    const query = singleUserQuery(id);
-    const userVideosQuery = userCreatedPostsQuery(id);
-    const userLikedVideosQuery = userLikedPostsQuery(id);
+    const data = await client.fetch(query);
 
-    const user = await client.fetch(query);
-    const userVideos = await client.fetch(userVideosQuery);
-    const userLikedVideos = await client.fetch(userLikedVideosQuery);
+    res.status(200).json(data[0]);
+  } else if (req.method === 'PUT') {
+    const { comment, userId } = req.body;
 
-    const data = { user: user[0], userVideos, userLikedVideos };
+    const { id }: any = req.query;
+
+    const data = await client
+      .patch(id)
+      .setIfMissing({ comments: [] })
+      .insert('after', 'comments[-1]', [
+        {
+          comment,
+          _key: uuid(),
+          postedBy: { _type: 'postedBy', _ref: userId },
+        },
+      ])
+      .commit();
 
     res.status(200).json(data);
   }
