d2c59f47ae481780729c57202e8e7fd69950ac06
medandhala <medandhala@gmail.com>
2022-08-03 16:41:15 +0100

    Fix Style App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useState, useEffect } from 'react'
-import NavBar from '../components/NavBar'
-import SideBar from '../components/SideBar'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import '../styles/globals.css';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [isSSR, setIsSSR] = useState(true)
+  const [isSSR, setIsSSR] = useState(true);
+
   useEffect(() => {
-    setIsSSR(false)
-  }
-  , [])
-  if (isSSR) return null
+    setIsSSR(false);
+  }, []);  
+
+  if (isSSR) return null;
 
   return (
-  <GoogleOAuthProvider 
-    clientId="434572216963-icbfnhc7c7bonv2ukmkscv8j6kec0rm9.apps.googleusercontent.com"
-    >
-      <NavBar />
-      <div className="flex gap-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          <SideBar />
-        </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-          <Component {...pageProps} />
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20 '>
+          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+            <Component {...pageProps} />
           </div>
         </div>
+      </div>
     </GoogleOAuthProvider>
-  )
-  
-}
+  );
+};
 
-
-export default MyApp
+export default MyApp;
